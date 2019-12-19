f5db590bcd82e418f3dd897abf9265da8630e42c
專業技術組 <tech@dpublishing.org.tw>
2019-12-19 17:33:04 +0800

    將指引本文的 EPUB 內容提取到根目錄

--- pack-all.sh
+++ pack-all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+java -jar ~/epubcheck-4.2.2/epubcheck.jar ./tdpf-epub3guide -mode exp -save
 find samples/ ! -name 'style_template' -maxdepth 1 -mindepth 1 -type d  -print |  xargs -I '{}' java -jar ~/epubcheck-4.2.2/epubcheck.jar "{}" -mode exp -save
 find practices/ ! -name 'style_template' -maxdepth 1 -mindepth 1 -type d  -print |  xargs -I '{}' java -jar ~/epubcheck-4.2.2/epubcheck.jar "{}" -mode exp -save
